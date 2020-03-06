9260cd6703c6bb29f3acf959e4aedeb8dbb2fa12
vikash <vikashraj144@gmail.com>
2020-03-06 14:25:24 +0530

    comman component added

---
+++ src/components/Header/index.js
@@ -0,0 +1,19 @@
+import React, { useEffect }from "react"
+
+/**
+ * Footer Component
+ * @constant
+ * @type {function}
+ * @returns {JSX}
+ */
+const Header = () => {
+
+    return (
+        <div>
+            Header
+        </div>
+    )
+
+}
+
+export default Header;
---
+++ src/pages/company.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+
+
+
+const CompanyPage = () => {
+
+  return (
+    <div>
+      company
+      <Footer />
+      <Header />
+    </div>
+  ) 
+}
+
+export default CompanyPage
